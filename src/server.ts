cd95c723149c71e6f527ca676f28e0a87021a172
Ednardo Gomes <ednardo@email.com>
2024-04-11 21:03:04 -0300

    feat: validating event already registered.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import { generateSlug } from "./utils/generate-slug";
 
 const app = fastify();
 
@@ -13,17 +14,32 @@ app.post("/events", async (request, reply) => {
     title: z.string().min(4),
     details: z.string().nullable(),
     maximumAttendees: z.number().int().positive().nullable(),
-    slug: z.string().min(4),
   });
 
-  const data = createEventSchema.parse(request.body);
+  const { title, details, maximumAttendees } = createEventSchema.parse(
+    request.body
+  );
+
+  const slug = generateSlug(title);
+
+  const eventWithSameSlug = await prisma.event.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (eventWithSameSlug !== null) {
+    reply
+      .status(400)
+      .send(new Error("Another event with same title already exists."));
+  }
 
   const event = await prisma.event.create({
     data: {
-      title: data.title,
-      details: data.details,
-      maximumAttendees: data.maximumAttendees,
-      slug: data.slug,
+      title,
+      details,
+      maximumAttendees,
+      slug,
     },
   });
 
